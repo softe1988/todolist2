e6cd405e72ae28cc488f7b28baeb621b8e2eeba6
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-09 17:31:36 +0000

    working edit page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
+  before_action :all_tasks, only: [:index, :create, :update]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # index and show actions removed
-
+  def update
+     @task.update(task_params) 
+  end  
+    
   def new
     @task = Task.new
   end
